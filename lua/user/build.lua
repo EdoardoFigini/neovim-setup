2ae119cb1c24e23398a4e1278a5af56c055bc5f9
Edoardo Figini <edoardoeffe4@gmail.com>
2025-03-11 11:09:23 +0100

    added debug to build

--- lua/user/build.lua
+++ lua/user/build.lua
@@ -110,9 +110,57 @@ function M.config()
   vim.cmd("Exec " .. command(cmd, dir))
 end
 
+function M.dbg()
+  local cmd = neoconf.get("build.debug.cmd")
+  local dir = neoconf.get("build.debug.dir")
+  if not dir then
+    dir = "."
+  end
+
+  if not cmd then
+    vim.notify("No debug command found in NeoConf", vim.log.levels.ERROR)
+    return
+  end
+
+  set_execenv()
+  vim.cmd("Exec " .. command(cmd, dir))
+end
+
+function M.build_and_debug()
+  local build_cmd = neoconf.get("build.cmd")
+  local debug_cmd = neoconf.get("build.debug.cmd")
+  local bdir = neoconf.get("build.dir")
+  local rdir = neoconf.get("build.debug.dir")
+  if not bdir then
+    bdir = "."
+  end
+  if not rdir then
+    rdir = "."
+  end
+
+  if not build_cmd then
+    vim.notify("No build command found in NeoConf", vim.log.levels.ERROR)
+    return
+  end
+
+  if not debug_cmd then
+    vim.notify("No debug command found in NeoConf", vim.log.levels.ERROR)
+    return
+  end
+
+  local build = command(build_cmd, bdir)
+  local debug = command(debug_cmd, rdir)
+
+  set_execenv()
+  vim.cmd("Exec " .. build .. " && " .. debug)
+end
+
+
 vim.api.nvim_create_user_command("BuildProject", M.build, {})
 vim.api.nvim_create_user_command("RunProject", M.run, {})
+vim.api.nvim_create_user_command("DebugProject", M.dbg, {})
 vim.api.nvim_create_user_command("BuildAndRunProject", M.build_and_run, {})
+vim.api.nvim_create_user_command("BuildAndDebugProject", M.build_and_debug, {})
 vim.api.nvim_create_user_command("CleanProject", M.clean, {})
 vim.api.nvim_create_user_command("ConfigProject", M.config, {})
 
