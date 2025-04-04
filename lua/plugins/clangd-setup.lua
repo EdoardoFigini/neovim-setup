f4ccaf3916282748c1833bb9c6be6e58f1851760
Edoardo Figini <edoardoeffe4@gmail.com>
2025-04-04 16:02:47 +0200

    edit clangd setup

--- lua/plugins/clangd-setup.lua
+++ lua/plugins/clangd-setup.lua
@@ -1,12 +1,19 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        clangd = {
-          cmd = { "clangd", "--compile-commands-dir=build" },
-        },
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				clangd = {
+					cmd = {
+						"clangd",
+						"--compile-commands-dir=build",
+						"--log=error",
+					},
+					init_options = {
+						clangdFileStatus = true,
+					},
+				},
+			},
+		},
+	},
 }
